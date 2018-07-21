974caa236146f60d8804ab8b27a2b8dc7662932a
nyuyz518 <lilyzhangyun@gmail.com>
2018-07-21 19:03:29 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'welcome', to: 'static#hello_world'
+  get 'welcome', to: 'static#welcome'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
